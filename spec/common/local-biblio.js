2a222013cd28e52157fc4dc38f8f1fb98ee7661d
Gregg Kellogg <gregg@greggkellogg.net>
2023-06-01 13:38:37 -0700

    Update common files.

--- spec/common/local-biblio.js
+++ spec/common/local-biblio.js
@@ -1,4 +1,14 @@
 var localBibliography = {
+  CBD: {
+    "title": "CBD - Concise Bounded Description",
+    "href": "https://www.w3.org/Submission/CBD/",
+    "authors": [
+      "Patrick Stickler, Nokia"
+    ],
+    "rawDate": "2005-06-03",
+    "publisher": "W3C",
+    "status": "W3C Member Submission"
+  },
   ISO24707: {
     id: "ISO24707",
     title: "Information technology — Common Logic (CL) — A framework for a family of logic-based languages",
@@ -37,6 +47,12 @@ var localBibliography = {
     date: "1987",
     publisher: "ACM New York"
   },
+  SAX: {
+    title: "SAX – The Simple API for XML",
+    href: "https://accu.org/journals/overload/7/34/cornish_515/",
+    authors: ["Steve Cornish"],
+    date: "October 1999",
+  },
   STRIPEDRDF: {
     id: "STRIPEDRDF",
     title: "RDF: Understanding the Striped RDF/XML Syntax",
@@ -46,49 +62,18 @@ var localBibliography = {
     status: "Team Submission",
     publisher: "W3C"
   },
-  "RDF12-CONCEPTS": {
-    "authors": [
-        "Richard Cyganiak",
-        "David Wood",
-        "Markus Lanthaler"
+  "RDF-STAR-CG": {
+    title: "RDF-star and SPARQL-star",
+    href: "https://w3c.github.io/rdf-star/cg-spec/2021-12-17.html",
+    authors: [
+      "Olaf Hartig",
+      "Pierre-Antoine Champin",
+      "Gregg Kellogg",
+      "Andy Seaborne"
     ],
-    "href": "https://w3c.github.io/rdf-concepts/spec/",
-    "title": "RDF 1.2 Concepts and Abstract Syntax",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-SEMANTICS": {
-    "authors": [
-        "Patrick Hayes",
-        "Peter Patel-Schneider"
-    ],
-    "href": "https://w3c.github.io/rdf-semantics/spec/",
-    "title": "RDF 1.2 Semantics",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-N-QUADS": {
-    "authors": [
-        "Gavin Carothers"
-    ],
-    "href": "https://w3c.github.io/rdf-n-quads/spec/",
-    "title": "RDF 1.2 N-Quads",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-N-TRIPLES": {
-    "authors": [
-        "Gavin Carothers",
-        "Andy Seaborne"
-    ],
-    "href": "https://w3c.github.io/rdf-n-triples/spec/",
-    "title": "RDF 1.2 N-Triples",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
+    rawDate: "2021-12-17",
+    status: "Final Community Group Report",
+    publisher: "W3C"
   },
   "RDF12-NEW": {
     "authors": [
@@ -111,51 +96,7 @@ var localBibliography = {
     "status": "DNOTE",
     "publisher": "W3C"
   },
-  "RDF12-SCHEMA": {
-    "authors": [
-        "Dan Brickley",
-        "Ramanathan Guha"
-    ],
-    "href": "https://w3c.github.io/rdf-schema/spec/",
-    "title": "RDF 1.2 Schema",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-TRIG": {
-    "authors": [
-        "Gavin Carothers",
-        "Andy Seaborne"
-    ],
-    "href": "https://w3c.github.io/rdf-trig/spec/",
-    "title": "RDF 1.2 TriG",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-TURTLE": {
-    "authors": [
-        "Eric Prud'hommeaux",
-        "Gavin Carothers"
-    ],
-    "href": "https://w3c.github.io/rdf-turtle/spec/",
-    "title": "RDF 1.2 Turtle",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "RDF12-XML": {
-    "authors": [
-        "Fabien Gandon",
-        "Guus Schreiber"
-    ],
-    "href": "https://w3c.github.io/rdf-xml/spec/",
-    "title": "RDF 1.2 XML Syntax",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-NEW": {
+ "SPARQL12-NEW": {
     "authors": [
         "The W3C RDF-star Working Group"
     ],
@@ -174,125 +115,5 @@ var localBibliography = {
     "rawDate": "2022-01-01",
     "status": "WD",
     "publisher": "W3C"
-  },
-  "SPARQL12-QUERY": {
-    "authors": [
-        "Steven Harris",
-        "Andy Seaborne"
-    ],
-    "href": "https://w3c.github.io/sparql-query/spec/",
-    "title": "SPARQL 1.2 Query Language",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-UPDATE": {
-    "authors": [
-        "Paula Gearon",
-        "Alexandre Passant",
-        "Axel Polleres"
-    ],
-    "href": "https://w3c.github.io/sparql-update/spec/",
-    "title": "SPARQL 1.2 Update",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-SERVICE-DESCRIPTION": {
-    "authors": [
-        "Gregory Williams"
-    ],
-    "href": "https://w3c.github.io/sparql-service-description/spec/",
-    "title": "SPARQL 1.2 Service Description",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-FEDERATED-QUERY": {
-    "authors": [
-        "Eric Prud'hommeaux",
-        "Carlos Buil Aranda"
-    ],
-    "href": "https://w3c.github.io/sparql-federated-query/spec/",
-    "title": "SPARQL 1.2 Federated Query",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-RESULTS-JSON": {
-    "authors": [
-        "Andy Seaborne"
-    ],
-    "href": "https://w3c.github.io/sparql-results-json/spec/",
-    "title": "SPARQL 1.2 Query Results JSON Format",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-RESULTS-CSV-TSV": {
-    "authors": [
-        "Andy Seaborne"
-    ],
-    "href": "https://w3c.github.io/sparql-results-csv-tsv/spec/",
-    "title": "SPARQL 1.2 Query Results CSV and TSV Formats",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-RESULTS-XML": {
-    "authors": [
-        "Sandro Hawke",
-        "Dave Beckett",
-        "Jeen Broekstra"
-    ],
-    "href": "https://w3c.github.io/sparql-results-xml/spec/",
-    "title": "SPARQL 1.2 Query Results XML Formats",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-ENTAILMENT": {
-    "authors": [
-        "Birte Glimm",
-        "Chimezie Ogbuji"
-    ],
-    "href": "https://w3c.github.io/sparql-entailment/spec/",
-    "title": "SPARQL 1.2 Entailment Regimes",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-PROTOCOL": {
-    "authors": [
-        "Lee Feigenbaum",
-        "Gregory Williams",
-        "Kendall Clark",
-        "Elias Torres"
-    ],
-    "href": "https://w3c.github.io/sparql-protocol/spec/",
-    "title": "SPARQL 1.2 Protocol",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "SPARQL12-GRAPH-STORE-PROTOCOL": {
-    "authors": [
-        "Chimezie Ogbuji"
-    ],
-    "href": "https://w3c.github.io/sparql-graph-store-protocol/spec/",
-    "title": "SPARQL 1.2 Graph Store HTTP Protocol",
-    "rawDate": "2022-01-01",
-    "status": "WD",
-    "publisher": "W3C"
-  },
-  "CBD": {
-    "title": "CBD - Concise Bounded Description",
-    "href": "https://www.w3.org/Submission/CBD/",
-    "authors": [
-      "Patrick Stickler, Nokia"
-    ],
-    "rawDate": "2005-06-03",
-    "publisher": "W3C",
-    "status": "W3C Member Submission"
   },
 };
